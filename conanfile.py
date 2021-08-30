2ff80e6e0ae8cfa02ad33f277b12bb055d3e3758
Ralf Schulze <ralf.schulze@philips.com>
2021-08-30 11:52:21 +0200

    Update to libtirpc v1.3.2

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ import os
 
 class LibtirpcConan(ConanFile):
     name = "libtirpc"
-    version = "1.1.4"
+    version = "1.3.2"
     description = "Libtirpc is a port of Suns Transport-Independent RPC library to Linux"
     topics = ("conan", "tirpc", "RPC", "sun", "transport", "independent")
     url = "https://github.com/bincrafters/conan-libtirpc"
@@ -33,7 +33,7 @@ class LibtirpcConan(ConanFile):
         url = "{homepage}/files/{name}/{version}/{name}-{version}.tar.bz2".format(
             homepage=self.homepage, name=self.name, version=self.version
         )
-        sha256 = "2ca529f02292e10c158562295a1ffd95d2ce8af97820e3534fe1b0e3aec7561d"
+        sha256 = "e24eb88b8ce7db3b7ca6eb80115dd1284abc5ec32a8deccfed2224fc2532b9fd"
         tools.get(url, sha256=sha256)
         os.rename("{}-{}".format(self.name, self.version), self._source_subfolder)
 
